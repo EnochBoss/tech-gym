bb818875e9e51530fb6d321f24d46a53095a53b2
EnochBoss <enoch.bosomtwe@gmail.com>
2025-08-20 19:55:38 +0000

    add QuestionCard, QuizStart and ScoreSummary components and render them conditionally in app.jsx

--- tech-gym/src/App.jsx
+++ tech-gym/src/App.jsx
@@ -6,7 +6,7 @@ import ScoreSummary from './components/ScoreSummary'
 // import questions from './data/questions'
 
 function App() {
-  const [showQuizStart, setShowQuizStart] = useState(false);
+  const [showQuizStart, setShowQuizStart] = useState(true);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScoreSummary, setShowScoreSummary] = useState(false);
 
